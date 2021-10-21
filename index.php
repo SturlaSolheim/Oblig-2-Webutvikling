4664aadeabc0a01250cc2804856a63aed0a53e36
SturlaSolheim <90689441+SturlaSolheim@users.noreply.github.com>
2021-10-21 14:37:06 +0200

    Etter første økt

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Webutvikling</title>
 </head>
 <body>
     <nav class="navbox">
 
-            <div class="navigasjon">Hjem</div>
-            <div class="navigasjon">NAV test</div>
+            <div class="navigasjon"> <a href="index.php"> Hjem</a></div>
+            <div class="navigasjon"><a href="nav_wcag.php">NAV WCAG 2.0 test</a></div>
             <div class="navigasjon">Videoer</div>
             <div class="navigasjon">annet</div>
 
