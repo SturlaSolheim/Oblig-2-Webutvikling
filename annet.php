7051d222c5b6070b6cc7d5e5364559a994fac8b1
SturlaSolheim <90689441+SturlaSolheim@users.noreply.github.com>
2021-11-01 14:17:26 +0100

    smårusk

--- annet.php
+++ annet.php
@@ -5,6 +5,34 @@ include("header.php");
 
 
 
+<article class="pensum">
+<h1 class="pensumh1"> Gestaltlovene </h1>
+<p>
+    Gestaltlovene, er lover om form som tar for seg hvordan mennesker tolker og oppfatter helhetlige visuelle intrykk
+    De forskjellige gestaltlovene handler om:<br> </p>
+    <ul>
+        <li>Forgrunn og bakgrunn</li> <br>
+        <li>Nærhet</li> <br>
+        <li>Likhet</li> <br>
+        <li>Sammenkoblinger</li> <br>
+        <li>Symmetri</li> <br>
+        <li>Kontinuitet</li> <br>
+        <li>Lukkethet</li> <br>
+    </ul>
+
+<p>
+    
+</p>
+
+
+
+
+
+
+</article>
+
+
+
 
 
 
--- header.php
+++ header.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Webutvikling</title>
+    <title>Accessibility Police</title>
+    <link rel="icon" type="image/gif/png" href="bilder/logo.png">
+    
 </head>
 <body>
     <div class="wrapper">
